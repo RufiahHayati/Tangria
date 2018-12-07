a343ba19d466216511ed9d8ce25384919e69e04e
RufiahHayati <rufiah122@gmail.com>
2018-12-07 12:53:37 +0700

    update

--- app/src/main/java/comw/example/rplrus26/tangria/SpashScreen.java
+++ app/src/main/java/comw/example/rplrus26/tangria/SpashScreen.java
@@ -1,13 +1,32 @@
 package comw.example.rplrus26.tangria;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class SpashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_spash_screen);
+        setTitle("");
+
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+
+            }
+        },  3000L);
+
     }
 }
